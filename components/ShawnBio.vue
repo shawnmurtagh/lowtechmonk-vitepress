643dda0451d21b866a39026dbf40b8ce82bf5c21
Shawn Murtagh <smurtagh@foundationsoft.com>
2025-08-27 00:07:58 -0400

    add blog components

---
+++ components/ShawnBio.vue
@@ -0,0 +1,17 @@
+<template>
+  <bio
+    name="Shawn Murtagh"
+    description="Son. Husband. Father of 4. Programmer. Writer. Seeker of slow wisdom in a world gone mad with speed. Founder of Low Tech Monk."
+    image-path="../public/assets/shawn-murtagh.jpg"
+  />
+</template>
+
+<script>
+import Bio from './bio.vue';
+
+export default {
+  components: {
+    Bio,
+  },
+};
+</script>
